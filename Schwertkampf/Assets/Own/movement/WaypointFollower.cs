6b9bbb943fb86c81ba19ab3be28afbd53344a25e
David <miner2.blabl@gmail.com>
2024-06-27 19:40:43 +0200

    Waypointfollower works now

--- Schwertkampf/Assets/Own/movement/WaypointFollower.cs
+++ Schwertkampf/Assets/Own/movement/WaypointFollower.cs
@@ -16,28 +16,38 @@ public class PathFollower : MonoBehaviour
     public float reachThreshold = 0.1f;
     private int currentWaypointIndex = 0;
     private bool isMoving = true; // Flag to control movement
-    
+    private PlayerStates playerStates;
+
+    void Start()
+    {
+        playerStates = GameObject.FindGameObjectWithTag("XROrigin").GetComponent<PlayerStates>();
+    }
+
     void Update()
     {
-        if (!isMoving || waypointManager.waypoints.Length == 0)
-            return;
+        if (!playerStates.inCombat)
+                        {
+            if (!isMoving || waypointManager.waypoints.Length == 0)
+                return;
 
-        Transform targetWaypoint = waypointManager.waypoints[currentWaypointIndex];
-        Vector3 direction = targetWaypoint.position - transform.position;
-        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
-        if (!SoundEffectsManager.instance.footstepAudioSource.isPlaying)
-        {
-            SoundEffectsManager.instance.footstepAudioSource.Play();
-        }
-        if (Vector3.Distance(transform.position, targetWaypoint.position) < reachThreshold)
-        {
-            currentWaypointIndex++;
-            if (currentWaypointIndex >= waypointManager.waypoints.Length)
+            Transform targetWaypoint = waypointManager.waypoints[currentWaypointIndex];
+            Vector3 direction = targetWaypoint.position - transform.position;
+            transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+            if (!SoundEffectsManager.instance.footstepAudioSource.isPlaying)
             {
-                isMoving = false; // Stop moving when the last waypoint is reached
-                SoundEffectsManager.instance.footstepAudioSource.Stop();
+                SoundEffectsManager.instance.footstepAudioSource.Play();
+            }
+            if (Vector3.Distance(transform.position, targetWaypoint.position) < reachThreshold)
+            {
+                currentWaypointIndex++;
+                if (currentWaypointIndex >= waypointManager.waypoints.Length)
+                {
+                    isMoving = false; // Stop moving when the last waypoint is reached
+                    SoundEffectsManager.instance.footstepAudioSource.Stop();
+                }
             }
         }
+       
     }
 }
 
